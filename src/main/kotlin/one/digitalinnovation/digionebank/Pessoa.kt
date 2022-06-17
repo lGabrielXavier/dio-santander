e9400f069b38ec82b09d417fbbfaa7b8db184112
perkles <bielxd2@hotmail.com>
2022-06-17 05:58:35 -0300

    update to talk abaut class members

--- src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -5,14 +5,16 @@ class Pessoa {
     var nome: String = "gabriel"
     var cpf: String = "132.000.124-19"
     private set
-}
 
+    constructor()
+
+    fun pessoaInfo() ="$nome e $cpf"
+}
 fun main() {
 
     val gabriel = Pessoa()
+    println(gabriel.pessoaInfo())
 
-    println(gabriel.nome)
-    println(gabriel.cpf)
 
 
 }
